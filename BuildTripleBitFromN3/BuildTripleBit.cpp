c03a08074d895909d639875a9578b5682c25b825
妖精么么 <1054550112@qq.com>
2019-05-05 17:11:53 +0800

    buildtriplebit

--- BuildTripleBitFromN3/BuildTripleBit.cpp
+++ BuildTripleBitFromN3/BuildTripleBit.cpp
@@ -9,22 +9,32 @@
 #include "../TripleBit/OSFile.h"
 
 char* DATABASE_PATH;
+char* columns;
 int main(int argc, char* argv[])
 {
-	if(argc != 3) {
-		fprintf(stderr, "Usage: %s <N3 file name> <Database Directory>\n", argv[0]);
+	//默认数据集格式为3列，btc4列
+	if (argc < 3 || argc >4) {
+		fprintf(stderr, "Usage: %s <N3 file name> <Database Directory> [--columnNum]\n", argv[0]);
 		return -1;
 	}
 
-	if(OSFile::directoryExists(argv[2]) == false) {
+	if (OSFile::directoryExists(argv[2]) == false) {
 		OSFile::mkdir(argv[2]);
 	}
-
+	if (argc > 3)
+		columns = argv[3];
+	else
+		columns = NULL;
 	DATABASE_PATH = argv[2];
-	TripleBitBuilder* builder = new TripleBitBuilder(argv[2]);
-	builder->startBuildN3(argv[1]);
-	builder->endBuild();
- 	delete builder;
+	TripleBitBuilder* builder = new TripleBitBuilder(argv[2]);//建立4个统计索引文件,uri的6个文件，pre的6个文件以及temp的4个文件,并将SINGLE写入字典中
 
- 	return 0;
+	cout << "start to store" << endl;
+	builder->beforeBuildforNum(argv[1]);// 开始解析
+
+
+	//builder->startBuildN3(argv[1]);
+	builder->endBuild();
+	delete builder;
+
+	return 0;
 }
